167f42689b66b8d94d98be0ecdfe6f12367b3246
Jack Hyc <Jacek.Hyc@gmail.com>
2018-03-13 16:40:29 -0500

    Adding finished project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using DbConnection;
 
 namespace quotingDojo.Controllers
 {
@@ -10,10 +11,28 @@ namespace quotingDojo.Controllers
     {
         // GET: /Home/
         [HttpGet]
-        [Route("")]
+        [Route("/")]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        [Route("quotes")]
+        public IActionResult quotes()
+        {
+            List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM users order by created_at desc");
+            ViewBag.users = AllQuotes;
+            return View();
+        }
+        [HttpPost]
+        [Route("createQuotes")]
+        public IActionResult createQuotes(string name, string quote)
+        {
+            string dbname = '"' + name + '"';
+            string dbquote = '"' + quote + '"';
+            string query = $"INSERT INTO users(name, quote, created_at) VALUES({dbname}, '{dbquote}', NOW());";
+            DbConnector.Execute(query);
+            return RedirectToAction("quotes");
         }
     }
 }
